84745b417ac5ca2fee8077fb9b100e8cba30f940
kalehub <=>
2021-04-24 23:05:33 +0800

    initiate moving pieces

--- scratch.py
+++ scratch.py
@@ -19,7 +19,8 @@ def main():
     LIGHT_BROWN = '#D3B38C'
 
     imgs = 'imgs/whites/bird.png'
-    piecesPos = [0,240]
+    piecesPos_x = 0
+    piecesPos_y = 280
 
     size = (340,340)
     screen = pygame.display.set_mode(size)
@@ -29,19 +30,31 @@ def main():
     clock = pygame.time.Clock()
     total = 0
 
+
     while is_not_closed:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                is_not_closed = False
-        screen.fill(WHITE)        
+        is_clicked = False
         pawns = pygame.image.load('imgs/whites/pawns-01.png').convert_alpha()
+        #pawns = pygame.image.load('imgs/whites/pawns-01.png').convert_alpha()
         pawns = pygame.transform.scale(pawns, (40, 40)) 
-        pygame.draw.rect(screen, LIGHT_BROWN, [0, 280, 40, 40], 0)
+        # pygame.draw.rect(screen, LIGHT_BROWN, [0, 280, 40, 40], 0)
 
         pygame.draw.rect(screen, LIGHT_BROWN, [0, 280, 40, 40], 0)
         pygame.draw.rect(screen, DARK_BROWN, [0, 240, 40, 40], 0)
-        screen.blit(pawns,(0,280))
+        screen.blit(pawns,(piecesPos_x, piecesPos_y))
         pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_not_closed = False
+            if event.type == pygame.MOUSEMOTION:
+                if event.buttons[0]:
+                    piecesPos_x+= event.rel[0]
+                    piecesPos_y+= event.rel[1]
+            
+        screen.fill(WHITE)        
+
+
+            
+            
         # pygame.display.update()
 
 
